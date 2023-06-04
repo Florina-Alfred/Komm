a042112a0e6fb84faf100cb0b31896f4f2db851c
Florina-Alfred <sonofcop@gmail.com>
2023-06-04 19:20:56 +0200

    entering code

--- consumer/app/main.py
+++ consumer/app/main.py
@@ -1,1 +1,22 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
+import json
+
+consumer = KafkaConsumer(
+    "foobar",
+    bootstrap_servers="kafka.default.svc.cluster.local",
+    auto_offset_reset="latest",
+    # consumer_timeout_ms=5000,
+    value_deserializer=json.loads,
+    group_id=None,
+)
+
+# consumer.assign(TopicPartition(topic="foobar", partition=0))
+# consumer.seek_to_end()
+# consumer.poll()
+print("_" * 20)
+
+print(next(consumer), "_" * 20)
+for msg in consumer:
+    print(msg.value)
+
+# print(next(consumer))
--- producer/app/main.py
+++ producer/app/main.py
@@ -1,1 +1,16 @@
 from kafka import KafkaProducer
+import uuid
+import json
+import time
+
+producer = KafkaProducer(
+    bootstrap_servers=["kafka-0.kafka-headless.default.svc.cluster.local:9092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
+
+for i in range(1000):
+    v = str(uuid.uuid4())
+    print(v)
+    producer.send("foobar", {"id": v, "time": str(time.time())})
+    time.sleep(1)
+producer.flush()
