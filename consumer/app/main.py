39b0a8a154be635e7fcb89197aa9962f69a993c5
Florina-Alfred <sonofcop@gmail.com>
2023-06-05 18:21:31 +0200

    camera

--- consumer/app/main.py
+++ consumer/app/main.py
@@ -1,32 +1,45 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 import uvicorn
 from kafka import KafkaConsumer, TopicPartition
 import json
 import os
+import cv2
+import numpy as np
 
 app = FastAPI()
 
 # load env variables
 BROKER_SERVER = os.getenv("BROKER_SERVER", "localhost:9092")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "foobar")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "webcam")
 
 
-@app.get("/")
-async def root():
+def gen_frame():
     consumer = KafkaConsumer(
         KAFKA_TOPIC,
         bootstrap_servers=BROKER_SERVER,
         # bootstrap_servers="kafka.default.svc.cluster.local",
         auto_offset_reset="latest",
         # consumer_timeout_ms=5000,
-        value_deserializer=json.loads,
-        enable_auto_commit=True,
+        # value_deserializer=json.loads,
+        enable_auto_commit=False,
         group_id=None,
     )
     # consumer.poll()
     # consumer.seek_to_end()
     # consumer.seek_to_end(TopicPartition(KAFKA_TOPIC, 0))
-    return next(consumer).value
+
+    for msg in consumer:
+        nparr = np.frombuffer(msg.value, np.uint8)
+        frame = nparr.tobytes()
+        yield (b"--frame\r\n" b"Content-Type: image\r\n\r\n" + frame + b"\r\n")
+
+
+@app.get("/")
+async def root():
+    return StreamingResponse(
+        gen_frame(), media_type="multipart/x-mixed-replace; boundary=frame"
+    )
 
 
 uvicorn.run(app, host="0.0.0.0", port=8000)
--- producer/app/main.py
+++ producer/app/main.py
@@ -3,6 +3,7 @@ import uuid
 import json
 import time
 import os
+import cv2
 
 # load env variables
 BROKER_SERVER = os.getenv("BROKER_SERVER", "localhost:9092")
@@ -11,13 +12,20 @@ KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "foobar")
 producer = KafkaProducer(
     bootstrap_servers=[BROKER_SERVER],
     # bootstrap_servers=["kafka-0.kafka-headless.default.svc.cluster.local:9092"],
-    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    # value_serializer=lambda v: json.dumps(v).encode("utf-8"),
 )
 
-
+vid = cv2.VideoCapture(0)
 while True:
-    v = str(uuid.uuid4())
-    print(v)
-    producer.send(KAFKA_TOPIC, {"id": v, "time": str(time.time())})
-    time.sleep(1)
+    ret, frame = vid.read()
+    cv2.imshow("frame", frame)
+
+    ret, buffer = cv2.imencode(".jpg", frame)
+    producer.send("webcam", buffer.tobytes())
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
 producer.flush()
