53e00e23a677cf1d59b3ffbd08879cf9445fbc92
Florina-Alfred <sonofcop@gmail.com>
2023-06-08 12:07:18 +0200

    typo

--- producer/app/main.py
+++ producer/app/main.py
@@ -10,7 +10,7 @@ BROKER_SERVER = os.getenv("BROKER_SERVER", "localhost:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "foobar")
 KAFKA_COMPRESSION = os.getenv("KAFKA_COMPRESSION", "gzip")
 KAFKA_COMPRESSION = os.getenv("KAFKA_COMPRESSION", "gzip")
-CV2_ENCODE_SCALE = int(os.getenv("CV2_IMAGE_SCALE", "75"))
+CV2_ENCODE_SCALE = int(os.getenv("CV2_ENCODE_SCALE", "75"))
 CV2_IMAGE_SCALE = int(os.getenv("CV2_IMAGE_SCALE", "3"))
 
 producer = KafkaProducer(
